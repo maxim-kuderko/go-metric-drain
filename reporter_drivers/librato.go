95a1559c7aed1b5f8adff6e550a0504a7076a05d
Maxim Kudekro <kuderko@gmail.com>
2018-03-27 14:50:01 +0300

    send aggregated data to librato driver

--- reporter_drivers/librato.go
+++ reporter_drivers/librato.go
@@ -17,7 +17,7 @@ type LibratoDriver struct {
 
 func NewLibratoDriver(token string) *LibratoDriver {
 	enc := base64.StdEncoding
-	return &LibratoDriver{url: "https://api.appoptics.com/v1/measurements", token: "Basic " + enc.EncodeToString([]byte(token))}
+	return &LibratoDriver{url: "https://api.appoptics.com/v1/measurements", token: "Basic " + enc.EncodeToString([]byte(token + ":"))}
 }
 
 func (ld *LibratoDriver) Send(name string, Points [][2]int64, tags map[string]string) {
@@ -44,31 +44,48 @@ func (ld *LibratoDriver) Send(name string, Points [][2]int64, tags map[string]st
 
 func (ld *LibratoDriver) jsonify(name string, Points [][2]int64, tags map[string]string) []byte {
 	metrics := make([]*libratoMetric, 0, 1000)
+	mp := map[int64]*libratoMetric{}
 	for _, p := range Points {
-		m := &libratoMetric{
-			Name:   name,
-			Time:   p[1] - (p[1] % 60),
-			Period: 60,
-			Count:  p[0],
-			Tags: tags,
+		key := p[0] - p[0] % 60
+		v, ok := mp[key]
+		if !ok{
+			mp[key] = &libratoMetric{}
+			v = mp[key]
+			v.Name = name
+			v.Period = 60
+			v.Time = key
 		}
-		metrics = append(metrics, m)
+		v.Count++
+		v.Sum += p[1]
+		v.Last = p[1]
+		if p[1] < v.Min{
+			v.Min = p[1]
+		}
+		if p[1] > v.Max{
+			v.Max = p[1]
+		}
 	}
-	ds := libratoRequest{Measurements: metrics}
+	for _, v := range mp{
+		metrics = append(metrics, v)
+	}
+	ds := libratoRequest{Measurements: metrics, Tags: tags,}
 	b, _ := json.Marshal(ds)
 	return b
 
 }
 
 type libratoRequest struct {
-	Measurements []*libratoMetric `json:"measurements"`
+	Measurements []*libratoMetric  `json:"measurements"`
+	Tags         map[string]string `json:"tags"`
 }
 
 type libratoMetric struct {
-	Name   string            `json:"name"`
-	Time   int64             `json:"time"`
-	Period int               `json:"period"`
-	Sum    float64           `json:"sum"`
-	Count  int64               `json:"count"`
-	Tags   map[string]string `json:"tags"`
+	Name   string `json:"name"`
+	Time   int64  `json:"time"`
+	Period int64  `json:"period"`
+	Sum    int64  `json:"sum"`
+	Count  int64  `json:"count"`
+	Min    int64  `json:"min"`
+	Max    int64  `json:"max"`
+	Last   int64  `json:"last"`
 }
