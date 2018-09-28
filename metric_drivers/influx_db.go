ed60c92061739c759976e9c9f313543224af5d51
Maxim Kuderko <kuderko@gmail.com>
2018-09-28 11:11:44 +0300

    + send one request to influx

--- metric_drivers/influx_db.go
+++ metric_drivers/influx_db.go
@@ -3,6 +3,7 @@ package metric_drivers
 import (
 	"github.com/influxdata/influxdb/client/v2"
 	"log"
+	"sync"
 	"time"
 )
 
@@ -12,19 +13,29 @@ type InfluxDB struct {
 	precision               string
 	retention               string
 	aggregationResolution   time.Duration
+	buff                    client.BatchPoints
+	sendBuffer              int
+	lastSend                time.Time
+	s sync.Mutex
 }
 
-func NewInfluxDB(url, username, password, database, precision, retention string, aggregationResolution time.Duration) *InfluxDB {
+func NewInfluxDB(url, username, password, database, precision, retention string, aggregationResolution time.Duration, sendBuffer int) *InfluxDB {
 	if aggregationResolution <= time.Second {
 		aggregationResolution = time.Second
 	}
-	return &InfluxDB{url: url, username: username, password: password, precision: precision, database: database, retention: retention, aggregationResolution: aggregationResolution}
+	return &InfluxDB{url: url, username: username, password: password, precision: precision, database: database, retention: retention, aggregationResolution: aggregationResolution, sendBuffer: sendBuffer, lastSend: time.Now()}
 }
 
 func (ifdb *InfluxDB) Send(key string, name string, Points []PtDataer, tags *map[string]string) error {
+	ifdb.s.Lock()
+	defer ifdb.s.Unlock()
 	batchPoints, err := ifdb.buildBatch(name, ifdb.aggregatePoints(Points), tags)
 	if err != nil {
 		return err
+	}
+	if len(batchPoints.Points()) < 1000 && time.Now().Sub(ifdb.lastSend).Seconds() < 10{
+		log.Println("*")
+		return nil
 	}
 	c, err := client.NewHTTPClient(client.HTTPConfig{
 		Addr:     ifdb.url,
@@ -36,16 +47,25 @@ func (ifdb *InfluxDB) Send(key string, name string, Points []PtDataer, tags *map
 		log.Fatal(err)
 	}
 	defer c.Close()
-	if err := c.Write(batchPoints); err != nil {
+	ifdb.lastSend = time.Now()
+	bp, err := ifdb.initBatch()
+	if err != nil{
+		ifdb.buff = nil
 		return err
 	}
+	ifdb.buff = bp
+	go func() {
+		c.Write(batchPoints)
+		log.Println(".")
+	}()
+
 	return nil
 }
 
 func (ifdb *InfluxDB) aggregatePoints(Points []PtDataer) map[time.Time]*AggregatedPoint {
 	mp := map[time.Time]*AggregatedPoint{}
 	for _, p := range Points {
-		key := time.Unix(0, p.Time().Add(-1*time.Duration(p.Time().UnixNano()%int64(ifdb.aggregationResolution.Nanoseconds()))).UnixNano())
+		key := time.Unix(0, p.Time().Add(-1 * time.Duration(p.Time().UnixNano()%int64(ifdb.aggregationResolution.Nanoseconds()))).UnixNano())
 		if _, ok := mp[key]; !ok {
 			mp[key] = &AggregatedPoint{
 				sum:   p.Data(),
@@ -75,24 +95,31 @@ type AggregatedPoint struct {
 }
 
 func (ifdb *InfluxDB) buildBatch(name string, Points map[time.Time]*AggregatedPoint, tags *map[string]string) (client.BatchPoints, error) {
-	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-		Database:        ifdb.database,
-		Precision:       ifdb.precision,
-		RetentionPolicy: ifdb.retention,
-	})
-	if err != nil {
-		return nil, err
+	if ifdb.buff == nil {
+		bp, err := ifdb.initBatch()
+		if err != nil {
+			return nil, err
+		}
+		ifdb.buff = bp
 	}
-
 	for t, point := range Points {
 		t.Add(time.Duration(time.Now().Nanosecond()))
 		p, err := client.NewPoint(name, *tags, map[string]interface{}{`count`: point.count, `sum`: point.sum, `min`: point.min, `max`: point.max, `last`: point.last}, t)
 		if err != nil {
 			continue
 		}
-		bp.AddPoint(p)
+		ifdb.buff.AddPoint(p)
 	}
 	Points = nil
 
+	return ifdb.buff, nil
+}
+
+func (ifdb *InfluxDB) initBatch() (client.BatchPoints, error) {
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:        ifdb.database,
+		Precision:       ifdb.precision,
+		RetentionPolicy: ifdb.retention,
+	})
 	return bp, err
 }
