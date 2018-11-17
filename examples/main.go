f0aab076209ab010b3267d1f57a23de30d4ce937
Max Kuderko <kuderko@gmail.com>
2018-11-17 13:05:18 +0200

    + add examples

---
+++ examples/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/maxim-kuderko/metric-reporter"
+	"github.com/maxim-kuderko/metric-reporter/metric_drivers"
+	"log"
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	/*go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()*/
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop)
+
+	ldriver := metric_drivers.NewMetricsLogger(`influxdb-telemtry`, "", ``)
+	cdriver := metric_drivers.NewMysqlCounter("root:getalife@tcp(localhost:3306)/metrics", "counters", 130)
+
+	reporter, err := metric_reporter.NewMetricsReporter(
+		[]metric_drivers.DriverInterface{ldriver},
+		[]metric_drivers.DriverInterface{cdriver},
+		1, 10000, time.Minute*30, "metric-logger-driver",
+		map[string]string{"env": "test"},
+	)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			reporter.Send("mytest.metric1", 1, map[string]string{"test": "test1"})
+			reporter.Send("mytest.metric1", 1, map[string]string{"test": "test2"})
+		}
+		log.Println("done")
+	}()
+
+	go func() {
+		for e := range err {
+			log.Println(e)
+		}
+	}()
+
+	<-stop
+	log.Println("sigint")
+	log.Println("waiting")
+	reporter.Wait() // wait for the reporter to exit
+	log.Println("exit")
+}
